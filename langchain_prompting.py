c1b103fb263ccf6d03476f9a4aa9f626300a7576
gatech <23293632+pratiksha-pai@users.noreply.github.com>
2023-10-14 14:22:19 -0400

    cleanup

--- langchain_prompting.py
+++ langchain_prompting.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
-from langchain.llms import OpenAI
 from langchain.globals import set_llm_cache
 from langchain.cache import InMemoryCache
-from langchain.prompts import PromptTemplate, ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import create_extraction_chain
-from langchain.callbacks import get_openai_callback
 
 app = Flask(__name__)
 
@@ -36,23 +33,9 @@ def process():
 
     value = data.get('key', '')
 
-    # chat_template = ChatPromptTemplate.from_messages([
-    #     ("human", f'So your friend is saying this about how they felt yesterday. "{value}". You need to follow up how they are feeling about it today. You are asking this as a follow up so when you ask how they are feeling about it today, you need to ask it in the past tense. Your follow up should be consice, empathetic and kind')
-    # ])
-
     chat_template = ChatPromptTemplate.from_messages([
         ("human", f'Yesterday, your friend shared this: "{value}". Today, kindly and empathetically ask them a brief follow-up question to understand how they feel about it now.')
     ])
-
-    # chat_template = ChatPromptTemplate.from_messages([
-    #     ("human", f'Yesterday, your friend shared this: "{value}". Acknowledge their situation or emotion first, then ask a brief follow-up question about how they feel today.')
-    # ])
-
-    # chat_template = ChatPromptTemplate.from_messages([
-    #     ("human", f'Yesterday, your friend shared this: "{value}". Acknowledge their situation or emotion first, then ask a brief follow-up question about how they feel today.')
-    # ])
-
-
 
     messages = chat_template.format_messages(value=value)
     response = llm(messages)
